127981da38f6ac3d1986952b429794a83de2dfa1
lenovo tab <132371436+lenovotab10@users.noreply.github.com>
2024-11-11 01:30:41 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,24 @@
-from flask import Flask, request, jsonify
-import pickle 
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
+from Transcribe import Transcribe
+import pickle 
 import re
 import nltk
+import os
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from Transcribe import Transcribe
-
-app = Flask(__name__)
-CORS(app)
-
-
-with open('svm_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-with open('tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
-    tfidf = pickle.load(vectorizer_file)
-
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
-
 stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
+
+
+client=os.path.join(os.getcwd(),"..","app","dist")
+
+
+app = Flask(__name__)
+CORS(app)
 
 def preprocess_text(text):
     text = text.lower()
@@ -29,22 +26,29 @@ def preprocess_text(text):
     tokens = [lemmatizer.lemmatize(word) for word in text.split() if word not in stop_words]
     return ' '.join(tokens)
 
+
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename="index.html"
+    return send_from_directory(client,filename)
+
 @app.post('/predict')
 def predict():
+    with open('svm_model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
+    with open('tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
+        tfidf = pickle.load(vectorizer_file)
+        
     input_text = request.json.get("input")
-
     if not input_text:
         return jsonify({"error": "No input text provided"}), 400
     
     processed_text = preprocess_text(input_text)
     vectorized_text = tfidf.transform([processed_text])
     prediction = model.predict(vectorized_text)
-    # print(f"Input Text: {input_text}\nProcessed Text: {processed_text}\nPrediction: {prediction}")
     return jsonify({"prediction": prediction[0]})
-
-@app.get("/audio/convert")
-def test():
-    return jsonify({"message": "Hello, World!"})
 
 @app.post('/audio/convert')
 def audio():
@@ -54,7 +58,6 @@ def audio():
     audiopath = './audios/' + audiofile.filename
     audiofile.save(audiopath)
     return {"text": Transcribe(audiopath)}
-    # return jsonify({"text": "hello"})
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5000)
+    app.run(debug=True)
